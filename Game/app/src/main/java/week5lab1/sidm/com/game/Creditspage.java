183b749e83a999f0c8022b456054f3ef6ca12083
Scrub <lookahorse12@gmail.com>
2017-11-21 09:51:11 +0800

    Dank Episode 3

---
+++ Game/app/src/main/java/week5lab1/sidm/com/game/Creditspage.java
@@ -0,0 +1,10 @@
+package week5lab1.sidm.com.game;
+
+import android.app.Activity;
+
+/**
+ * Created by User on 21/11/2017.
+ */
+
+public class Creditspage extends Activity{
+}
--- Game/app/src/main/java/week5lab1/sidm/com/game/Gamepage.java
+++ Game/app/src/main/java/week5lab1/sidm/com/game/Gamepage.java
@@ -1,8 +1,18 @@
 package week5lab1.sidm.com.game;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+
 /**
  * Created by User on 19/11/2017.
  */
 
-class Gamepage {
+class Gamepage extends Activity
+{
+
 }
