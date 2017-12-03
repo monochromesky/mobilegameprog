af263e058818580a83d48762ef1efa11005ccbbe
Scrub <lookahorse12@gmail.com>
2017-12-03 19:53:04 +0800

    added background, i think

--- Game/app/src/main/java/week5lab1/sidm/com/game/SampleBackground.java
+++ Game/app/src/main/java/week5lab1/sidm/com/game/SampleBackground.java
@@ -1,5 +1,4 @@
 package week5lab1.sidm.com.game;
-
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -9,6 +8,7 @@ public class SampleBackground implements EntityBase
 {
 
     private Bitmap bmp = null;
+    private Bitmap clouds = null;
     private boolean isDone = false;
 
     private float xPos, yPos, offset;
@@ -29,26 +29,31 @@ public class SampleBackground implements EntityBase
     @Override
     public void Init(SurfaceView _view)
     {
-        bmp = BitmapFactory.decodeResource(_view.getResources(), R.drawable.gamescene);
-         offset = 0.0f;
-         view = _view;
+        // bmp = BitmapFactory.decodeResource(_view.getResources(), R.drawable.gamescene);
+        bmp = BitmapFactory.decodeResource(_view.getResources(), R.drawable.taintskybg);
+        clouds = BitmapFactory.decodeResource(_view.getResources(), R.drawable.hearthp);
+
+        offset = 0.0f;
+        view = _view;
     }
 
     @Override
     public void Update(float _dt)
     {
-         offset += _dt * 0.1f;
+        offset += _dt * 0.8f;
     }
 
     @Override
     public void Render(Canvas _canvas)
     {
-         xPos = 0.5f * view.getWidth();
-         yPos = 0.5f * view.getHeight();
+        xPos = 0.5f * view.getWidth();
+        yPos = 0.5f * view.getHeight();
 
-         float xOffset = (float)Math.sin(offset) * bmp.getWidth() * 0.3f;
+        //float xOffsetbg = (float)Math.sin(offset) * bmp.getWidth() * 0.3f;
+        _canvas.drawBitmap(bmp, xPos - view.getWidth() * 0.5f, yPos - view.getHeight() * 0.5f, null);
 
-          _canvas.drawBitmap(bmp, xPos - bmp.getWidth() * 0.5f + xOffset, yPos - bmp.getHeight() * 0.5f, null);
+        float xOffsetcloud = (float)Math.sin(offset) * clouds.getWidth() * 0.3f;
+        _canvas.drawBitmap(clouds, xPos - clouds.getWidth() * 0.5f + xOffsetcloud, yPos - clouds.getHeight() * 0.5f, null);
     }
 
     public static SampleBackground Create()
